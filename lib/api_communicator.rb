401a29a1a65f18c570b0117f761a8fffd630855d
Peter Meister <prmeister89@gmail.com>
2019-02-17 19:58:44 +0000

    Done.

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -1,12 +1,19 @@
-require 'rest-client'
-require 'json'
 require 'pry'
+require 'rest-client'
+# require 'net/http'
+# require 'open-uri'
+# require 'json'
 
 def get_character_movies_from_api(character)
   #make the web request
+
+  # uri = URI('http://www.swapi.co/api/people/')
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
   character_hash = JSON.parse(all_characters)
-  
+  character_data = character_hash["results"].find { |data| data["name"] == character }
+  film_urls = character_data["films"]
+  film_data = film_urls.collect { |data| JSON.parse(RestClient.get(data)) }
+  film_data
   # iterate over the character hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
@@ -20,14 +27,14 @@ end
 
 def parse_character_movies(films_hash)
   # some iteration magic and puts out the movies in a nice list
+  # films_hash = get_character_movies_from_api("Luke Skywalker")
+  films_hash.each.with_index(1) do |data, index|
+    puts "#{index} " + data['title']
+  end
 end
 
 def show_character_movies(character)
   films_hash = get_character_movies_from_api(character)
   parse_character_movies(films_hash)
+  # binding.pry
 end
-
-## BONUS
-
-# that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
-# can you split it up into helper methods?
